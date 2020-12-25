2c08c89ae0cc8e984f73c26344a378fb133abaae
pmedina <1amedinapablo@gmail.com>
2020-12-25 19:36:50 +0100

    Finished home section

--- src/App.js
+++ src/App.js
@@ -8,22 +8,29 @@ import { Image, Row, Col } from 'react-bootstrap';
 import Navbar from './components/Navbar';
 
 // Import images
-
+import profilePhoto from './images/FotoPabloMedina.png';
 
 function App() {
+  console.log(profilePhoto)
   return (
     <Container fluid>
       <div id="main">
           <Navbar />
           <div id="home">
-            <img alt="Photo of Pablo" src={require('./images/FotoPabloMedina.png')} />
-            <div id="greetings">
-              <h1>Hi, I'm Pablo</h1>
-              <h3>Full Stack Developer with knowledge in TensorFlow</h3>
-            </div>
+            <Container id="image-container">
+              <Row>
+                <Col xs={6} md={4}>
+                  <Image src={profilePhoto} alt="Photo of Pablo" roundedCircle thumbnail fluid />
+                </Col>
+                <Col xs={10} md={8} id="greetings">
+                  <h1 className="fluid">Hi, I'm Pablo</h1>
+                  <h3>Full Stack Developer with knowledge in TensorFlow</h3>
+                </Col>
+              </Row>
+            </Container>
           </div>
-      </div>
-    </Container>
+        </div>
+      </Container>
   )
 }
 
