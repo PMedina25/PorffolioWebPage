177c70d65df92276c7061f02de055de011267512
pmedina <1amedinapablo@gmail.com>
2020-12-26 12:35:05 +0100

    Added about me info

--- src/App.js
+++ src/App.js
@@ -15,20 +15,44 @@ function App() {
   return (
     <Container fluid>
       <div id="main">
+
           <Navbar />
-          <div id="home">
-            <Container id="image-container">
-              <Row>
-                <Col xs={6} md={4}>
-                  <Image src={profilePhoto} alt="Photo of Pablo" roundedCircle thumbnail fluid />
-                </Col>
-                <Col xs={10} md={8} id="greetings">
-                  <h1 className="fluid">Hi, I'm Pablo</h1>
-                  <h3>Full Stack Developer with knowledge in TensorFlow</h3>
-                </Col>
-              </Row>
-            </Container>
-          </div>
+
+          <Container id="home" fluid>
+            <Row>
+              <Col md={4}>
+                <Image id="profile-image" src={profilePhoto} alt="Photo of Pablo" roundedCircle thumbnail fluid />
+              </Col>
+              <Col md={8} id="greetings">
+                <h1>Hi, I'm Pablo</h1>
+                <h4>Full Stack Developer with knowledge in TensorFlow</h4>
+              </Col>
+            </Row>
+          </Container>
+
+          <Container id="about-me" fluid>
+             <Col md={6} id="about-me-info">
+               <h1>About Me</h1>
+               <p>Computer engineer, full stack developer  and tech enthusiast.</p>
+               <p>With experience in teamwork, always willing to learn new things and delighted at facing new challenges.</p>
+               <p>Looking for a MERN Stack junior developer job, especially if it involves Machine Learning.</p>
+             </Col>
+             <Col md={6} id="about-me-grid">
+               <Image className="image-item" />
+               <Image className="image-item" />
+               <Image className="image-item" />
+               <Image className="image-item" />
+               <Image className="image-item" />
+               <Image className="image-item" />
+               <Image className="image-item" />
+               <Image className="image-item" />
+               <Image className="image-item" />
+             </Col>
+          </Container>
+
+          <Container id="skills">
+            
+          </Container>
         </div>
       </Container>
   )
