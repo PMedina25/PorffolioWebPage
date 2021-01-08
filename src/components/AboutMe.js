9351360ce3081f29eeff1d0364ecf3f2e5355461
pmedina <1amedinapablo@gmail.com>
2021-01-08 09:58:09 +0100

    Added leaflet map

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,35 +1,11 @@
 import React, { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
-import mapboxgl from 'mapbox-gl';
+
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 import { Col, Row  } from 'react-bootstrap';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoicG1lZGluYTI1IiwiYSI6ImNrZmZoeWg1MDA0aWEyeG1reDN2dXJ1a3MifQ.odUV_vUZROT5EFn4-WlQDA';
-
 const AboutMe = () => {
-    const mapContainerRef = useRef(null);
-
-    const [lng, setLng] = useState(-0.5805000);
-    const [lat, setLat] = useState(44.8404400);
-    const [zoom, setZoom] = useState(3.25);
-
-    // Initialize map when component mounts
-    useEffect(() => {
-        const map = new mapboxgl.Map({
-            container: mapContainerRef.current,
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [lng, lat],
-            zoom: zoom
-        });
-
-
-        const markerSevilla = new mapboxgl.Marker()
-            .setLngLat([-5.97317, 37.38283])   
-            .addTo(map); 
-
-        // Clean up on unmount
-        return () => map.remove();
-    }, []);
 
     return (
         <Row id="about-me" className="card card-body bg-light">
@@ -40,8 +16,17 @@ const AboutMe = () => {
                 <p>Looking for a MERN Stack junior developer job, especially if it involves Machine Learning.</p>
             </Col>
             <Col md={6} id="about-me-map">
-                <div id="mapContainer" ref={mapContainerRef}>
-                </div>
+                <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} id="mapContainer">
+                    <TileLayer
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[51.505, -0.09]}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
             </Col>
         </Row> 
     )
