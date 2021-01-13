90a3b9775b2723e3ce27e5d49cbe390cddb724d4
pmedina <1amedinapablo@gmail.com>
2021-01-13 11:08:30 +0100

    Added media queries for project info

--- client/src/components/all-projects/Projects.js
+++ client/src/components/all-projects/Projects.js
@@ -28,9 +28,8 @@ const Projects = ({ projects }) => {
                             .filter(project => project.category === 'professional' || project.category === 'academic')
                             .map(project => {
                                 return (
-                                    <div className="card-column-item">
+                                    <div key={project.id} className="card-column-item">
                                         <ProjectCard
-                                                key={project.id}
                                                 title={project.title} 
                                                 description={project.description}
                                                 technologies={project.technologies}
@@ -54,9 +53,8 @@ const Projects = ({ projects }) => {
                         .filter(project => project.category === 'personal')
                         .map(project => {
                             return (
-                                <div className="card-column-item">
+                                <div key={project.id} className="card-column-item">
                                     <ProjectCard
-                                            key={project.id}
                                             title={project.title} 
                                             description={project.description}
                                             technologies={project.technologies}
@@ -80,9 +78,8 @@ const Projects = ({ projects }) => {
                         .filter(project => project.category === 'others')
                         .map(project => {
                             return (
-                                <div className="card-column-item">
+                                <div key={project.id} className="card-column-item">
                                     <ProjectCard
-                                            key={project.id}
                                             title={project.title} 
                                             description={project.description}
                                             technologies={project.technologies}
