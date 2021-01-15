e07b27fd628a546abe5f1afc1d089483e2064ae3
pmedina <1amedinapablo@gmail.com>
2021-01-15 14:26:39 +0100

    Added tabs for skills

--- client/src/pages/Skills.js
+++ client/src/pages/Skills.js
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 
 // Import bootstrap components
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Col, Container, Image, Row, Tab, Tabs } from 'react-bootstrap';
 
 // Import components
 import Sidebar from '../components/common/Sidebar';
@@ -33,6 +33,8 @@ import herokuIcon from '../assets/images/heroku.png';
 
 
 const Skills = () => {
+    const [value, setValue] = useState(0);
+
     useEffect(() => {
         Aos.init({duration: 1000});
     }, []);
@@ -42,101 +44,113 @@ const Skills = () => {
 
             <Sidebar />
 
-            <Row>
-                <Col xs={12} md={12} lg={6} id="skills-info">
-                    <h1 className="mb-4" data-aos="fade-up">Skills</h1>
-                    <p data-aos="fade-up">The main area of my expertise is all related to MERN Stack (<strong>MongoDB</strong>, <strong>Express</strong>, <strong>React</strong>, <strong>Node</strong>).</p>
-                    <p data-aos="fade-up">
-                        This involves skilled in <strong>HTML</strong>, <strong>CSS</strong>, <strong>JavaScript</strong> and <strong>JSX</strong>, 
-                        besides CSS frameworks such as <strong>Bootstrap</strong> and <strong>Semantic UI</strong>. For managing react states,
-                        I have developed some applications with <strong>React Hooks</strong>, <strong>Context API</strong> and <strong>Redux</strong>.
-                    </p>
-                    <p data-aos="fade-up">Moreover, experienced in developing Backend services with <strong>GraphQL</strong> and <strong>Apollo Server</strong>.</p>
-                    <p data-aos="fade-up">Finally, as a complementary background, <strong>Java</strong> for <strong>Android App Development</strong>, and also <strong>TensorFlow</strong> and <strong>Keras</strong> with <strong>Python</strong>.</p>
-                </Col>
-                <Col xs={12} md={12} lg={6} id="technologies-grid">
-                    <div className="technologies-grid-item" data-aos="fade-up">
-                        <Image className="technology-icon" src={htmlIcon} alt="html icon" />
-                        <p>HTML</p>
-                    </div>
-                    <div className="technologies-grid-item" data-aos="fade-up">
-                        <Image className="technology-icon" src={cssIcon} alt="css icon" />
-                        <p>CSS</p>
-                    </div>
-                    <div className="technologies-grid-item" data-aos="fade-up">
-                        <Image className="technology-icon" src={javascriptIcon} alt="javascript icon" />
-                        <p>JavaScript</p>
-                    </div>
-                    <div className="technologies-grid-item" data-aos="fade-up">
-                        <Image className="technology-icon" src={reactIcon} alt="react icon" />
-                        <p>React</p>
-                    </div>
-                    <div className="technologies-grid-item" data-aos="fade-up">
-                        <Image className="technology-icon" src={reduxIcon} alt="redux icon" />
-                        <p>Redux</p>
-                    </div>
-                    <div className="technologies-grid-item" data-aos="fade-up">
-                        <Image className="technology-icon" src={bootstrapIcon} alt="bootstrap icon" />
-                        <p>Bootstrap</p>
-                    </div>
-                    <div className="technologies-grid-item" data-aos="fade-up">
-                        <Image className="technology-icon" src={semanticUiIcon} alt="semantic ui icon" />
-                        <p>Semantic UI</p>
-                    </div>
-                    <div className="technologies-grid-item" data-aos="fade-up">
-                        <Image className="technology-icon" src={nodeIcon} alt="node icon" />
-                        <p>Node.js</p>
-                    </div>
-                    <div className="technologies-grid-item" data-aos="fade-up">
-                        <Image className="technology-icon" src={mongodbIcon} alt="mongodb icon" />
-                        <p>MongoDB</p>
-                    </div>
-                    <div className="technologies-grid-item" data-aos="fade-up">
-                        <Image className="technology-icon" src={graphqlIcon} alt="graphql icon" />
-                        <p>GraphQL</p>
-                    </div>
-                    <div className="technologies-grid-item" data-aos="fade-up">
-                        <Image className="technology-icon" src={apolloIcon} alt="apollo icon" />
-                        <p>Apollo Server</p>
-                    </div>
-                    <div className="technologies-grid-item" data-aos="fade-up">
-                        <Image className="technology-icon" src={pythonIcon} alt="python icon" />
-                        <p>Python</p>
-                    </div>
-                    <div className="technologies-grid-item" data-aos="fade-up">
-                        <Image className="technology-icon" src={tensorFlowIcon} alt="tensorflow icon" />
-                        <p>TensorFlow</p>
-                    </div>
-                    <div className="technologies-grid-item" data-aos="fade-up">
-                        <Image className="technology-icon" src={kerasIcon} alt="keras icon" />
-                        <p>Keras</p>
-                    </div>
-                    <div className="technologies-grid-item" data-aos="fade-up">
-                        <Image className="technology-icon" src={mySqlIcon} alt="mysql icon" />
-                        <p>MySQL</p>
-                    </div>
-                    <div className="technologies-grid-item" data-aos="fade-up">
+            <div className="fields-center">
+                <h1>Skills</h1>
+                <div className="underline"></div>
+
+                <div className="btn-container">
+                    <button onClick={() => setValue(0)} className={`field-btn ${value === 0 && 'active-btn'}`}>Frontend</button>
+                    <button onClick={() => setValue(1)} className={`field-btn ${value === 1 && 'active-btn'}`}>Backend</button>
+                    <button onClick={() => setValue(2)} className={`field-btn ${value === 2 && 'active-btn'}`}>Tools</button>
+                    <button onClick={() => setValue(3)} className={`field-btn ${value === 3 && 'active-btn'}`}>Embedded Systems</button>
+                    <button onClick={() => setValue(4)} className={`field-btn ${value === 4 && 'active-btn'}`}>A.I.</button>
+                </div>
+
+                {value === 0 &&
+                    <article id="frontend-content" className="field-content">
+                        <div className="technologies-grid-item" data-aos="zoom-in">
+                            <Image className="technology-icon" src={htmlIcon} alt="html icon" />
+                            <p>HTML</p>
+                        </div>
+                        <div className="technologies-grid-item" data-aos="zoom-in">
+                            <Image className="technology-icon" src={cssIcon} alt="css icon" />
+                            <p>CSS</p>
+                        </div>
+                        <div className="technologies-grid-item" data-aos="zoom-in">
+                            <Image className="technology-icon" src={javascriptIcon} alt="javascript icon" />
+                            <p>JavaScript</p>
+                        </div>
+                        <div className="technologies-grid-item" data-aos="zoom-in">
+                            <Image className="technology-icon" src={reactIcon} alt="react icon" />
+                            <p>React</p>
+                        </div>
+                        <div className="technologies-grid-item" data-aos="zoom-in">
+                            <Image className="technology-icon" src={reduxIcon} alt="redux icon" />
+                            <p>Redux</p>
+                        </div>
+                        <div className="technologies-grid-item" data-aos="zoom-in">
+                            <Image className="technology-icon" src={bootstrapIcon} alt="bootstrap icon" />
+                            <p>Bootstrap</p>
+                        </div>
+                        <div className="technologies-grid-item" data-aos="zoom-in">
+                            <Image className="technology-icon" src={semanticUiIcon} alt="semantic ui icon" />
+                            <p>Semantic UI</p>
+                        </div>
+                    </article>
+                }
+                {value === 1 &&
+                    <article id="backend-content" className="field-content">
+                        <div className="technologies-grid-item" data-aos="zoom-in">
+                            <Image className="technology-icon" src={nodeIcon} alt="node icon" />
+                            <p>Node.js</p>
+                        </div>
+                        <div className="technologies-grid-item" data-aos="zoom-in">
+                            <Image className="technology-icon" src={mongodbIcon} alt="mongodb icon" />
+                            <p>MongoDB</p>
+                        </div>
+                        <div className="technologies-grid-item" data-aos="zoom-in">
+                            <Image className="technology-icon" src={graphqlIcon} alt="graphql icon" />
+                            <p>GraphQL</p>
+                        </div>
+                        <div className="technologies-grid-item" data-aos="zoom-in">
+                            <Image className="technology-icon" src={apolloIcon} alt="apollo icon" />
+                            <p>Apollo Server</p>
+                        </div>
+                        <div className="technologies-grid-item" data-aos="zoom-in">
+                            <Image className="technology-icon" src={mySqlIcon} alt="mysql icon" />
+                            <p>MySQL</p>
+                        </div>
+                    </article>
+                }
+                {value === 2 &&
+                    <article id="tools-content" className="field-content">
+                        <div className="technologies-grid-item" data-aos="zoom-in">
+                            <Image className="technology-icon" src={gitIcon} alt="git icon" />
+                            <p>Git</p>
+                        </div>
+                        <div className="technologies-grid-item" data-aos="zoom-in">
+                            <Image className="technology-icon" src={githubIcon} alt="github icon" />
+                            <p>GitHub</p>
+                        </div>
+                        <div className="technologies-grid-item" data-aos="zoom-in">
+                            <Image className="technology-icon" src={dockerIcon} alt="docker icon" />
+                            <p>Docker</p>
+                        </div>
+                    </article>
+                }
+                {value === 3 &&
+                    <article id="embedded-systems-content" className="field-content">
                         <Image className="technology-icon" src={androidJavaIcon} alt="android java icon" />
                         <p>Java for Android</p>
-                    </div>
-                    <div className="technologies-grid-item" data-aos="fade-up">
-                        <Image className="technology-icon" src={gitIcon} alt="git icon" />
-                        <p>Git</p>
-                    </div>
-                    <div className="technologies-grid-item" data-aos="fade-up">
-                        <Image className="technology-icon" src={githubIcon} alt="github icon" />
-                        <p>GitHub</p>
-                    </div>
-                    <div className="technologies-grid-item" data-aos="fade-up">
-                        <Image className="technology-icon" src={dockerIcon} alt="docker icon" />
-                        <p>Docker</p>
-                    </div>
-                    <div className="technologies-grid-item" data-aos="fade-up">
-                        <Image className="technology-icon" src={herokuIcon} alt="heroku icon" />
-                        <p>Heroku</p>
-                    </div>
-                </Col>
-            </Row>
+                    </article>
+                }
+                {value === 4 &&
+                    <article id="articial-intelligence-content" className="field-content">
+                        <div className="technologies-grid-item" data-aos="zoom-in">
+                            <Image className="technology-icon" src={pythonIcon} alt="python icon" />
+                            <p>Python</p>
+                        </div>
+                        <div className="technologies-grid-item" data-aos="zoom-in">
+                            <Image className="technology-icon" src={tensorFlowIcon} alt="tensorflow icon" />
+                            <p>TensorFlow</p>
+                        </div>
+                        <div className="technologies-grid-item" data-aos="zoom-in">
+                            <Image className="technology-icon" src={kerasIcon} alt="keras icon" />
+                            <p>Keras</p>
+                        </div>
+                    </article>
+                }
+            </div>
         </Container>
     );
 }
